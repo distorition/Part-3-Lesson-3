0f8175649a6aa988a34a1b454f49a1ce1198ec6f
distorition <85959781+distorition@users.noreply.github.com>
2022-02-21 10:54:02 +0300

    Add files via upload

---
+++ Part 3 lesson 3/Part 3 lesson 3/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Part_3_lesson_3.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Part_3_lesson_3.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public PersonRepositories person;
+        public HomeController(PersonRepositories person1)
+        {
+            person = person1;
+        }
+
+        [HttpGet("read")]
+        public IEnumerable<Person> GetById([FromQuery]int personId)
+        {
+
+            return (IEnumerable<Person>)person.data.Where(x => x.Id == personId);
+        }
+
+        [HttpGet("person")]
+        public IEnumerable<Person> Skiptake([FromForm] int skip=5,[FromQuery] int take = 10)
+        {
+            return (IEnumerable<Person>)person.data.Skip(skip).Take(take);
+        }
+
+        [HttpGet("name")]
+        public IEnumerable<Person> GetName([FromQuery] string name)
+        {
+            return (IEnumerable<Person>)person.data.Where(x => x.FirstName == name);
+        }
+
+        [HttpPost("new person")]
+        public IActionResult NewPerson([FromQuery]string name,[FromQuery] string emael,[FromQuery]int age,[FromQuery] int id)
+        {
+            person.data.Add(new Person() { Age=age,Email=emael,FirstName=name,Id=id});
+            return View();
+        }
+
+        [HttpDelete("person delete")]
+        public IActionResult DeletePerson(int IdtoDelete)
+        {
+           for(int i = 0; i < person.data.Count; i++)
+            {
+                if (person.data[i].Id == IdtoDelete)
+                {
+                    person.data = person.data.Where(x => x.Id != IdtoDelete).ToList();
+                }
+            }
+            return View();
+        }
+
+        [HttpPut("update")]
+        public IActionResult PersonUpdtade([FromQuery]string value,int idForSearch, string email,[FromQuery]string name)
+        {
+            for (int i = 0; i < person.data.Count; i++)
+            {
+                if (person.data[i].Id == idForSearch)
+                {
+                    person.data[i].Email = email;
+                    person.data[i].FirstName = name;
+                }
+            }
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Repositories/PersonRepositories.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Part_3_lesson_3.Repositories
+{
+    public class PersonRepositories
+    {
+        public Person Person { get; set; }
+
+      public  List<Person> data = new List<Person>() {
+new Person { Id = 1, FirstName = "Veda", LastName = "Richmond", Email = "ligula@necluctus.edu", Company = "Quisque Ac Libero LLP", Age = 42 },
+new Person { Id = 2, FirstName = "Demetria", LastName = "Andrews", Email = "feugiat.metus@penatibuset.org", Company = "Nulla Facilisi Foundation", Age = 31 },
+new Person { Id = 3, FirstName = "Byron", LastName = "Holmes", Email = "neque.Sed.eget@non.co.uk", Company = "Et Associates", Age = 63 },
+new Person { Id = 4, FirstName = "Alexander", LastName = "Cummings", Email = "egestas.ligula@ultricesDuisvolutpat.ca", Company = "Vel Institute", Age = 23 } };
+
+    }
+}
